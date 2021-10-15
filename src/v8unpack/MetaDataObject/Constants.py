08aa6cdd93ef9142c0a70770b9b837ba327eacad
sergrud <sergrud@mail.ru>
2021-10-15 14:30:55 +0300

    Реализована поддержка распаковки Констант

--- src/v8unpack/MetaDataObject/Constants.py
+++ src/v8unpack/MetaDataObject/Constants.py
@@ -1,10 +1,10 @@
-#from ..MetaDataObject.core.SimpleWithInfo import SimpleWithInfo
 from ..MetaDataObject.core.Simple import Simple
 
-
 class Constants(Simple):
-    ext_code = {'obj': 0}  # модуль объекта Константа
-
+    ext_code = {
+        'mgr': '1',  # модуль менеджера Константы
+        'obj': '0',  # модуль менеджера значения Константы
+    }
     @classmethod
     def get_decode_header(cls, header_data):
         return header_data[0][1][1][1][1]
