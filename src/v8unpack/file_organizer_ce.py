85d524dd7dcebac378deccc6d75082f546d0d364
razgovorov <razgovorov@tensor.ru>
2021-11-08 18:15:43 +0300

    сборка с наследованием собралось и разобралось

--- src/v8unpack/file_organizer_ce.py
+++ src/v8unpack/file_organizer_ce.py
@@ -1,41 +1,50 @@
 import os
-import shutil
 
-from .file_organizer import FileOrganizer
 from . import helper
+from .file_organizer import FileOrganizer
 
 
 class FileOrganizerCE(FileOrganizer):
 
     @classmethod
-    def _unpack_file(cls, src_path, src_file_name, dest_path, dest_file_name, descent=None):
-        src_full_path = os.path.join(src_path, src_file_name)
-
+    def unpack_get_descent_filename(cls, src_path, src_file_name, src_data, dest_path, dest_file_name, descent,
+                                    comparer):
+        # если есть файл нашей версии или новый файл отличается от старого, или старого файла нет
+        # сохраняем файл с номером текущей версии
+        # считаем, что общие изменения должны делаться на минимальной версии
         descent_file_name = helper.get_descent_file_name(dest_file_name, descent)
         descent_full_dest_path = os.path.join(dest_path, descent_file_name)
 
         if not os.path.isfile(descent_full_dest_path):  # если нет файла нужной версии
-            near_descent_path, near_descent_file_name = helper.get_near_descent_file_name(dest_path, dest_file_name, descent)
+            near_descent_path, near_descent_file_name = helper.get_near_descent_file_name(dest_path, dest_file_name,
+                                                                                          descent)
             if near_descent_file_name:  # нашли файл младшей версии
-                with open(os.path.join(near_descent_path, near_descent_file_name), 'rb') as near_file:
-                    with open(src_full_path, 'rb') as src_file:
-                        near_data = near_file.read()
-                        src_data = src_file.read()
-                        if near_data == src_data:  # если файл не менялся, ничего с ним не делаем
-                            return
-
-        # если есть файл нашей версии или новый файл отличается от старого, или старого файла нет
-        # сохраняем файл с номером текущей версии
-        # считаем, что общие изменения должны делаться на минимальной версии
-        shutil.copy(src_full_path, descent_full_dest_path)
+                if comparer(src_path, src_file_name, src_data, near_descent_path, near_descent_file_name):
+                    return '', ''
+        return dest_path, descent_file_name
 
     @classmethod
-    def _pack_file(cls, src_path, src_file_name, dest_path, dest_file_name, descent=None):
-        _src_path = os.path.join(src_path, src_file_name)
-        _dest_path = os.path.join(dest_path, dest_file_name)
-        try:
-            shutil.copy(_src_path, _dest_path)
-        except FileNotFoundError:
-            _dest_dir = os.path.dirname(_dest_path)
-            os.makedirs(_dest_dir, exist_ok=True)
-            shutil.copy(_src_path, _dest_path)
+    def pack_get_descent_filename(cls, src_path, src_file_name, descent):
+        descent_path, descent_file_name = helper.get_near_descent_file_name(src_path, src_file_name, descent)
+        if not descent_file_name:
+            raise FileNotFoundError(f'{src_path}/{src_file_name} ({descent})')
+        return descent_path, descent_file_name
+
+    @classmethod
+    def list_descent_dir(cls, src_dir, path, descent):
+        _index = {}
+        result = []
+        _dir = os.path.join(src_dir, path)
+        entries = os.listdir(_dir)
+        for entry in entries:
+            full_path = os.path.join(_dir, entry)
+            if os.path.isdir(full_path):
+                result.append(entry)
+            else:
+                name: list = entry.split('.')
+                name.pop(-2)
+                entry_without_descent = '.'.join(name)
+                if entry_without_descent not in _index:
+                    _index[entry_without_descent] = 1
+                    result.append(entry_without_descent)
+        return result
