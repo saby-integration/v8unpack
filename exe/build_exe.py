8a904274a2281550866ba03e51541cfe214a5ead
razgovorov <razgovorov@tensor.ru>
2021-12-14 15:33:04 +0300

    fix сборка exe файла.

--- exe/build_exe.py
+++ exe/build_exe.py
@@ -1,12 +1,18 @@
 import PyInstaller.__main__
 
 from v8unpack import __version__
+# from PyInstaller.utils.hooks import collect_submodules
 
 _version = __version__.split('.')
+
+# modules = ' '.join(collect_submodules('v8unpack'))
 
 PyInstaller.__main__.run([
     'source.py',
     '--onefile',
-    '-n', f'v8unpack_{"_".join(_version[:-1])}',
+    # '-n', f'v8unpack_{"_".join(_version[:-1])}',
+    '-n', 'v8unpack',
+    # '--hidden-import', modules,
+    '--collect-submodules', 'v8unpack.MetaDataObject',
     '--distpath', '.'
 ])
