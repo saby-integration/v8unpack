e0ceadda97a62372588a61329ec3c024ecf468c2
razgovorov <razgovorov@tensor.ru>
2022-10-21 12:41:35 +0300

    ## 0.10.3 * существенное ускорение разбора скобкофайлов за счет того что вложенный 1с код теперь разбирается не по символьно а строками целиком. форма с 1Mb вложенного 1С кода раньше разбиралась 2 минуты, теперь 6 сек.

--- exe/build_exe.py
+++ exe/build_exe.py
@@ -1,17 +1,9 @@
 import PyInstaller.__main__
 
-# from v8unpack.version import __version__
-# from PyInstaller.utils.hooks import collect_submodules
-
-# _version = __version__.split('.')
-
-# modules = ' '.join(collect_submodules('v8unpack'))
-
 PyInstaller.__main__.run([
     'source.py',
     '--onefile',
     '-n', 'v8unpack',
-    # '--hidden-import', modules,
     '--collect-all', 'v8unpack',
     '--hidden-import', 'tqdm',
     '--hidden-import', 'os',
