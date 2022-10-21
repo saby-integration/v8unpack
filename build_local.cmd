923f95c50852eb8fc3210324399b4000cb1a1f97
razgovorov <razgovorov@tensor.ru>
2022-10-21 11:41:20 +0300

    0.10.2 fix модуль и exe-шник версий 0.10.0-1 собирался не полностью

--- build_local.cmd
+++ build_local.cmd
@@ -2,4 +2,4 @@ python -m build
 
 python -m pip install -e .
 
-pyinstaller ./exe/source.py --onefile --name v8unpack --distpath ./exe/ --collect-submodules v8unpack --collect-submodules v8unpack.MetaObject --collect-submodules v8unpack.MetaDataObject
+pyinstaller ./exe/source.py --onefile --name v8unpack --distpath ./exe/ --collect-all v8unpack --hidden-import tqdm
